7da997f513c343cd9a052b85935ce01e5e72b475
Maxim Bing <smiling77877@users.noreply.github.com>
2024-09-09 19:34:32 +0800

    修改bug

--- webook/interactive/wire_gen.go
+++ webook/interactive/wire_gen.go
@@ -20,27 +20,35 @@ import (
 // Injectors from wire.go:
 
 func InitApp() *App {
+	srcDB := ioc.InitSrcDB()
+	dstDB := ioc.InitDstDB()
 	loggerV1 := ioc.InitLogger()
-	db := ioc.InitDB(loggerV1)
+	doubleWritePool := ioc.InitDoubleWritePool(srcDB, dstDB, loggerV1)
+	db := ioc.InitBizDB(doubleWritePool)
 	interactiveDAO := dao.NewGORMInteractiveDAO(db)
 	cmdable := ioc.InitRedis()
 	interactiveCache := cache.NewInteractiveRedisCache(cmdable)
 	interactiveRepository := repository.NewCachedInteractiveRepository(interactiveDAO, loggerV1, interactiveCache)
 	client := ioc.InitSaramaClient()
 	interactiveReadEventConsumer := events.NewInteractiveReadEventConsumer(interactiveRepository, client, loggerV1)
-	v := ioc.InitConsumers(interactiveReadEventConsumer)
+	consumer := ioc.InitFixerConsumer(client, loggerV1, srcDB, dstDB)
+	v := ioc.InitConsumers(interactiveReadEventConsumer, consumer)
 	interactiveService := service.NewInteractiveService(interactiveRepository)
 	interactiveServiceServer := grpc.NewInteractiveServiceServer(interactiveService)
-	server := ioc.NewGrpcxServer(interactiveServiceServer)
+	server := ioc.NewGrpcxServer(interactiveServiceServer, loggerV1)
+	syncProducer := ioc.InitSaramaSyncProducer(client)
+	producer := ioc.InitInteractiveProducer(syncProducer)
+	ginxServer := ioc.InitGinxServer(loggerV1, srcDB, dstDB, doubleWritePool, producer)
 	app := &App{
-		consumers: v,
-		server:    server,
+		consumers:   v,
+		server:      server,
+		adminServer: ginxServer,
 	}
 	return app
 }
 
 // wire.go:
 
-var thirdPartySet = wire.NewSet(ioc.InitDB, ioc.InitLogger, ioc.InitSaramaClient, ioc.InitRedis)
+var thirdPartySet = wire.NewSet(ioc.InitSrcDB, ioc.InitDstDB, ioc.InitDoubleWritePool, ioc.InitBizDB, ioc.InitLogger, ioc.InitSaramaClient, ioc.InitSaramaSyncProducer, ioc.InitRedis)
 
 var interactiveSvcSet = wire.NewSet(dao.NewGORMInteractiveDAO, cache.NewInteractiveRedisCache, repository.NewCachedInteractiveRepository, service.NewInteractiveService)
